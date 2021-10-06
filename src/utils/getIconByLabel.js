528a601e181e446d4c400ef53228e976eb227c7f
Alexis G <merkkur39@gmail.com>
2021-10-06 10:09:53 +0200

    feat: Build test

--- src/utils/getIconByLabel.js
+++
@@ -1,7 +0,0 @@
-import People from 'cozy-ui/transpiled/react/Icons/People'
-
-const identityLabels = ['national_id_card', 'passport']
-
-export const getIconByLabel = label => {
-  if (identityLabels.includes(label)) return People
-}
