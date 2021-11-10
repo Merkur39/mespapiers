a286d611260684d96756da87b8676ed9fab86966
Alexis G <merkkur39@gmail.com>
2021-11-10 16:15:15 +0100

    publish: feat: Added service worker used by CRA
    generated from commit e288aa67ec2d830ae5fab427502c52fd00130a88

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-// This allows the web app to trigger skipWaiting via
-// registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener('message', event => {
-  console.log('event : ', event)
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
-  }
-})
-self.addEventListener('activate', event => {
-  console.log('activated', event)
-})
+// TODO
